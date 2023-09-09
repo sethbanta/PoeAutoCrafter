54b036ff580c52379c015bfedf3d42a9cadbf43a
sethbanta <sethbanta@u.boisestate.edu>
2023-09-09 14:36:14 -0600

    Added actual coordinates and modified mouse clicks
    Used the grabber to get the 1080p coordinates of transmutes, alterations, augments, regals, scours, exalts, and the showcase. Currently the script has only been tested crafting a jewel, other items will need some modification which I will add soonTM. Also adjusted the way the program handles moving the mouse and clicking because sometimes it would click too early and not grab the currency, or pick up the item randomly. Added a spot to pause the grabber so that I could properly write down the coordinates as well.

--- TransAltRegEx.py
+++ TransAltRegEx.py
@@ -25,14 +25,16 @@ def transmute():
     print(f'Transmuted')
     #pick up transmute, 100,100 will need to change to the location of wherever transmutes are 
     #in your resolution
-    pyautogui.rightClick(100,100,duration=0.5)
+    pyautogui.moveTo(55,268,duration=0.5)
+    pyautogui.rightClick()
     pyautogui.PAUSE
-    pyautogui.leftClick(100,100,duration=0.4)
+    pyautogui.moveTo(339,451,duration=0.4)
+    pyautogui.leftClick()
     #copy to clipboard and see if the item somehow hit what we want
-   # pyautogui.hotkey('alt','ctrl','c')
+    pyautogui.hotkey('alt','ctrl','c')
     pyautogui.PAUSE
     clipboardData = clippy.clipboard_get()
-    deez = clipboardData.split('--------')[6].split('\"')
+    deez = clipboardData.split('--------')[2].split('\"')
     #grab data from clipboard
     #split or truncate so that we find the mods on the item
     #search in an array if the mods that are on the item are what we want
@@ -46,23 +48,20 @@ def transmute():
 #Function used to perform alting an item
 def alteration():
     global alterationCount
-    #pick up alt
-    pyautogui.rightClick(100,100,duration=0.3)
-    pyautogui.PAUSE
-    #hold down shift
-    #use alt
     while (alterationCount > 0):
         print(f'Alted')
-        pyautogui.keyDown('shift')
-        pyautogui.leftClick(100,100,duration=0.1)
-        pyautogui.sleep(0.25)
+        #pick up alt
+        pyautogui.moveTo(118,267, duration=0.1)
+        pyautogui.rightClick()
+        #use alt
+        pyautogui.moveTo(339,451,duration=0.1)
+        pyautogui.leftClick()
         alterationCount = alterationCount - 1
         #grab item data
-        pyautogui.keyUp('shift')
-      #  pyautogui.hotkey('alt','ctrl','c')
+        pyautogui.hotkey('alt','ctrl','c')
         pyautogui.PAUSE
         clipboardData = clippy.clipboard_get()
-        deez = clipboardData.split('--------')[6].split('\"')
+        deez = clipboardData.split('--------')[2].split('\"')
         deezLen = len(deez)
         #if it matches, regal, if it doesnt, do nothing and keep rolling
         for str in deez:         
@@ -72,16 +71,17 @@ def alteration():
                 if(deezLen == 3):
                     #augment
                     print(f'augmented')
-                    pyautogui.rightClick(100,100,duration=0.25)
-                    pyautogui.leftClick(100,100,duration=0.25)
+                    pyautogui.moveTo(233,327,duration=0.25)
+                    pyautogui.rightClick()
+                    pyautogui.moveTo(339,451,duration=0.25)
+                    pyautogui.leftClick()
                 #check if it needs to be regaled or keep alting
-          #      pyautogui.hotkey('alt','ctrl','c')
+                pyautogui.hotkey('alt','ctrl','c')
                 clipboardData = clippy.clipboard_get()
-                deezTwo = clipboardData.split('--------')[6].split('\"')
+                deezTwo = clipboardData.split('--------')[2].split('\"')
                 for str in deezTwo:
                     if(modArray.count(str) and str != foundMod):
                         regal()
-    pyautogui.keyUp('shift')
     print(f'Reached alteration count, exiting')
     exit()
         
@@ -92,16 +92,18 @@ def alteration():
 def regal():
     print(f'Regaled')
     #grab regal
-    pyautogui.rightClick(100,100,duration=0.5)
+    pyautogui.moveTo(445,264,duration=0.5)
+    pyautogui.rightClick()
     pyautogui.PAUSE
     #use regal
-    pyautogui.leftClick(100,100,duration=0.4)
+    pyautogui.moveTo(339,451,duration=0.4)
+    pyautogui.leftClick()
     pyautogui.PAUSE
     #check mods
- #   pyautogui.hotkey('alt','ctrl','c')
+    pyautogui.hotkey('alt','ctrl','c')
     pyautogui.PAUSE
     clipboardData = clippy.clipboard_get()
-    deez = clipboardData.split('--------')[6].split('\"')
+    deez = clipboardData.split('--------')[2].split('\"')
     desiredMods = 0
     for str in deez:
         if(modArray.count(str) and (str != "Dangerous" or str != "of the Bear")):
@@ -115,10 +117,12 @@ def regal():
 def scour():
     print(f'Scoured')
     #pick up scour
-    pyautogui.rightClick(100,100,duration=0.3)
+    pyautogui.moveTo(436,505,duration=0.3)
+    pyautogui.rightClick()
     pyautogui.PAUSE
     #use scour
-    pyautogui.leftClick(100,100,duration=0.4)
+    pyautogui.moveTo(339,451,duration=0.4)
+    pyautogui.leftClick()
     pyautogui.PAUSE
     #back to transmuting
     transmute()
@@ -127,10 +131,12 @@ def scour():
 def exalt():
     print(f'Exalted')
     #pick up exalt
-    pyautogui.rightClick(100,100,duration=0.5)
+    pyautogui.moveTo(303,264,duration=0.5)
+    pyautogui.rightClick()
     pyautogui.PAUSE
     #use exalt
-    pyautogui.leftClick(100,100,duration=0.4)
+    pyautogui.moveTo(339,451,duration=0.4)
+    pyautogui.leftClick()
     pyautogui.PAUSE
     #at this point user will manually check what was slammed and get asked if they want to scour or not
     question = input('scour? (y/n) ')
